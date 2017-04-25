cfddd430bd65db4711dda9ea57fa2de3e2033e7c
skykeeper0 <trungdinh0429@gmail.com>
2017-04-24 22:11:31 -0500

    adding todoview and todoviewmodel

---
+++ src/index.js
@@ -0,0 +1,20 @@
+
+// import needed libraries
+import React from 'react'
+import {render} from 'react-dom'
+
+// import the view and the viewModel
+import {TodoView} from './TodoView.material'
+import {TodoViewModel} from './TodoViewModel'
+
+import injectTapEventPlugin from 'react-tap-event-plugin'
+
+// Needed for onTouchTap by material-ui
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+// create a viewModel singleton
+const model = new TodoViewModel()
+
+// render the editor
+render(<TodoView model={model} />, document.getElementById('root'))
